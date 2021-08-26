701a97b4413d70f083912a6859c84bcda8121728
GDLynx <lynxthatthinks@gmail.com>
2021-08-26 06:02:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+   await page.setDefaultNavigationTimeout(0);
   await page.goto('https://xshss.sse.codesandbox.io/lyrics?artist=disturbed&song=stricken');
   await page.screenshot({ path: 'example.png' });
   await page.waitForNavigation({
